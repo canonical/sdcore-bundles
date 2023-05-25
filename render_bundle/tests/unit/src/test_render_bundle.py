07ea619fb5869e6879cf82f0891469f0363a79b7
guillaume <guillaume.belanger27@gmail.com>
2023-05-25 10:30:25 -0400

    Uncomments unit test

--- render_bundle/tests/unit/src/test_render_bundle.py
+++ render_bundle/tests/unit/src/test_render_bundle.py
@@ -27,14 +27,13 @@ class TestRenderBundle(unittest.TestCase):
 
         self.assertEqual(file_content(rendered_bundle_path), file_content(expected_bundle_path))
 
+    def test_given_sdcore_user_plane_local_when_render_bundle_then_bundle_is_rendered(self):
+        rendered_bundle_path = f"{TEST_PATH}/rendered_bundle_sdcore_user_plane_local.yaml"
+        expected_bundle_path = f"{TEST_PATH}/expected_bundle_sdcore_user_plane_local.yaml"
+        render_bundle(
+            local=True,
+            bundle_variant="SDCORE_USER_PLANE",
+            output_file=rendered_bundle_path,
+        )
 
-#     def test_given_sdcore_user_plane_local_when_render_bundle_then_bundle_is_rendered(self):
-#         rendered_bundle_path = f"{TEST_PATH}/rendered_bundle_sdcore_user_plane_local.yaml"
-#         expected_bundle_path = f"{TEST_PATH}/expected_bundle_sdcore_user_plane_local.yaml"
-#         render_bundle(
-#             local=True,
-#             bundle_variant="SDCORE_USER_PLANE",
-#             output_file=rendered_bundle_path,
-#         )
-#
-#         self.assertEqual(file_content(rendered_bundle_path), file_content(expected_bundle_path))
+        self.assertEqual(file_content(rendered_bundle_path), file_content(expected_bundle_path))
